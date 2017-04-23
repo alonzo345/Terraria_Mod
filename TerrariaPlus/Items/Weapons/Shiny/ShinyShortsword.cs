00221110076a3bfeb5ed21f58e651ad9a55a6a30
Alon Zohar <alonzo345@gmail.com>
2017-04-23 22:05:09 +0300

    Added folder.

--- TerrariaPlus/Items/Weapons/Shiny/ShinyShortsword.cs
+++ TerrariaPlus/Items/Weapons/Shiny/ShinyShortsword.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace TerrariaPlus.Items
+namespace TerrariaPlus.Items.Weapons.Shiny
 {
 	public class ShinyShortsword : ModItem
 	{
