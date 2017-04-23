384f1e59013e4684acd6e1426160e4d0c1206a93
Alon Zohar <alonzo345@gmail.com>
2017-04-23 22:09:24 +0300

    Fixed Typo.

--- TerrariaPlus/Items/Tools/Rusty/RustyAxe.cs
+++ TerrariaPlus/Items/Tools/Rusty/RustyAxe.cs
@@ -36,8 +36,8 @@ namespace TerrariaPlus.Items.Tools.Rusty
 			recipe1.AddRecipe(); // Required.
 			
 			ModRecipe recipe2 = new ModRecipe(mod); // Required.
-			recipe2.AddIngredient(ItemID.TinAxe, 1 ); // The rquired ingredients.
-      			recipe2.AddIngredient(ItemID.Cobweb, 6 ); // The rquired ingredients.
+			recipe2.AddIngredient(ItemID.TinAxe, 1 ); // The required ingredients.
+      			recipe2.AddIngredient(ItemID.Cobweb, 6 ); // The required ingredients.
 			recipe2.AddTile(TileID.WorkBenches); // The rquired crafting station.
 			recipe2.SetResult(this); // The result of crafting.
 			recipe2.AddRecipe(); // Required.
