e165fdbae5eb1c583b0278d8f6d8624c4248d36e
Alon Zohar <alonzo345@gmail.com>
2017-04-23 20:10:49 +0300

    Fixed Crafting

--- TerrariaPlus/Items/RustyAxe.cs
+++ TerrariaPlus/Items/RustyAxe.cs
@@ -31,14 +31,14 @@ namespace TerrariaPlus.Items
 			ModRecipe recipe1 = new ModRecipe(mod); // Required.
 			recipe1.AddIngredient(ItemID.CopperAxe, 1 ); // The rquired ingredients.
       			recipe1.AddIngredient(ItemID.Cobweb, 6 ); // The rquired ingredients.
-			recipe1.AddTile(ItemID.WorkBench); // The rquired crafting station.
+			recipe1.AddTile(TileID.WorkBenches); // The rquired crafting station.
 			recipe1.SetResult(this); // The result of crafting.
 			recipe1.AddRecipe(); // Required.
 			
 			ModRecipe recipe2 = new ModRecipe(mod); // Required.
 			recipe2.AddIngredient(ItemID.TinAxe, 1 ); // The rquired ingredients.
       			recipe2.AddIngredient(ItemID.Cobweb, 6 ); // The rquired ingredients.
-			recipe2.AddTile(ItemID.WorkBenches); // The rquired crafting station.
+			recipe2.AddTile(TileID.WorkBenches); // The rquired crafting station.
 			recipe2.SetResult(this); // The result of crafting.
 			recipe2.AddRecipe(); // Required.
 		}
