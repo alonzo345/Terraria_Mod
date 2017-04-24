dfe3ae478cec15e5161954f1f3051245115e5c7a
Alon Zohar <alonzo345@gmail.com>
2017-04-24 21:16:03 +0300

    Bug Fixes.

--- TerrariaPlus/Items/Weapons/Dualsword/GoldDualsword.cs
+++ TerrariaPlus/Items/Weapons/Dualsword/GoldDualsword.cs
@@ -40,9 +40,15 @@ namespace TerrariaPlus.Items.Weapons.Dualsword
 				item.useTime = 10; // The amount of time it takes the item to be used.
 				item.useAnimation = 10; // The amount of time the animation is displayed.
 				item.useStyle = 3; // The use style of the item.
+				item.autoReuse = false; // "Auto swing"
 			}
 			else
 			{
+				item.damage = 15; // The amount of damage the item deals.
+				item.useTime = 19; // The amount of time it takes the item to be used.
+				item.useAnimation = 19; // The amount of time the animation is displayed.
+				item.useStyle = 1; // The use style of the item.
+				item.autoReuse = false; // "Auto swing"
 			}
 			return base.CanUseItem(player);
 		}
