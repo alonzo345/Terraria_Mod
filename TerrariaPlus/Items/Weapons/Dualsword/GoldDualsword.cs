5588b009a9b2489286f119217066a8abee8683ed
Alon Zohar <alonzo345@gmail.com>
2017-04-24 19:17:42 +0300

    First Version.

---
+++ TerrariaPlus/Items/Weapons/Dualsword/GoldDualsword.cs
@@ -0,0 +1,61 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerrariaPlus.Items
+{
+	public class GoldDualsword : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.name = "Gold Dualsword"; // The item display name in-game.
+			item.damage = 15; // The amount of damage the item deals.
+			item.melee = true; // Confirms that the item is indeed a melee item.
+			item.width = 16; // The width of the item in-game.
+			item.height = 16; // The height of the item in-game.
+			item.toolTip = "Left click to swing"; // The displayed tool-tip in-game.
+      item.toolTip2 = "Right click to stab"; // Another tool-tip.
+			item.useTime = 19; // The amount of time it takes the item to be used.
+			item.useAnimation = 19; // The amount of time the animation is displayed.
+			item.useStyle = 1; // The use style of the item.
+			item.knockBack = 5; // The knockback of the item.
+			item.value = Item.buyPrice(0, 2, 0, 0); // The items value in PGSC.
+			item.rare = 1; // The font color and rarity of the item.
+			item.UseSound = SoundID.Item1; // The sound the items make when used.
+			item.autoReuse = false; // "Auto swing"
+		}
+    
+    public override bool AltFunctionUse(Player player)
+		{
+			return true;
+		}
+    
+    // The "Right click" method.
+    public override bool CanUseItem(Player player)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				item.damage = 26; // The amount of damage the item deals.
+				item.useTime = 10; // The amount of time it takes the item to be used.
+				item.useAnimation = 10; // The amount of time the animation is displayed.
+				item.useStyle = 3; // The use style of the item.
+			}
+			else
+			{
+			}
+			return base.CanUseItem(player);
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod); // Required.
+			recipe.AddIngredient(ItemID.GoldShortsword, 1 ); // The rquired ingredients.
+      recipe.AddIngredient(ItemID.GoldBroadsword, 1 ); // The rquired ingredients.
+      recipe.AddIngredient(ItemID.GoldBar, 4 ); // The rquired ingredients.
+			recipe.AddTile(TileID.Anvils); // The rquired crafting station.
+			recipe.SetResult(this); // The result of crafting.
+			recipe.AddRecipe(); // Required.
+		}
+	}
+}
